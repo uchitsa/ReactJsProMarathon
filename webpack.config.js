2db174cab9e6f16f353655c9206536661a164b02
Anton K <kodi2020@ya.ru>
2020-10-28 19:09:59 +0300

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const HTMLWebpackPlugins = require("html-webpack-plugin");
 const NODE_ENV = process.env.NODE_ENV;
-
 module.exports = {
     resolve: {
         extensions: [".js",".jsx",".ts",".tsx",".json"]
