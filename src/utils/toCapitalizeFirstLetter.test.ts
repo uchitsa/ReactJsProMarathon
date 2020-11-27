df8a20647fc18f6bd47cdfe4f328175f6a6df014
Anton K <kodi2020@ya.ru>
2020-11-27 12:11:43 +0300

    Homework-8 WIP fix 1 mistake

--- src/utils/toCapitalizeFirstLetter.test.ts
+++ src/utils/toCapitalizeFirstLetter.test.ts
@@ -2,13 +2,8 @@ import toCapitalizeFirstLetter from "./toCapitalizeFirstLetter";
 
 describe('toCapitalizeFirstLetter', () => {
     test('Should capitalize first letter of the word', () => {
-        const url = toCapitalizeFirstLetter('word');
+        const result = toCapitalizeFirstLetter('word');
 
-        expect(url).toEqual({
-            protocol: 'http',
-            host: 'zar.hosthot.ru',
-            pathname: '/api/v1/pokemons',
-            query: {}
-        })
+        expect(result).toEqual('Word');
     })
 })
